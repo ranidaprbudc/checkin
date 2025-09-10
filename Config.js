0096355a50e9260167bb8dc07472d9ca1f3d0613
ranidaprbudc <ranida.pr.budc@cpird.in.th>
2025-09-10 16:32:30 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,9 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwf5jGjqaW_1wu-H8saJ1EUJuxuuI78HvG7IBqACAn0sG1ML9aw9xZK8mTvM-Pjybfi/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbxVegGtHiwdhSOLe4ZkwYJJODLg5rBQ4jlM3dU8LBAKWXQS4GFrlmIO6nFIu-WMH96-/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyka6ym4ASnS4uiW5YfucuzGYndLkyGguJZxK2lSv2vMFs59vjG_q_3K_WA9FIwM764/exec';
 //LIFF_ID
     const LIFF_ID = '2008088010-xldm1oo7'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008088010-Y1oOree3'; // LIFF ID history
     const LIFF_REGISTER = '2008088010-GkY6DVV2'; // LIFF ID register
+
 
